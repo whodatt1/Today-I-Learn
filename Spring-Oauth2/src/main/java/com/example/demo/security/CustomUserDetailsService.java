6bf5a1cbd2ef471ae662ec51089404c227530f26
whodatt1 <codingman875@naver.com>
2023-11-23 21:23:03 +0900

    update:2023-11-23 21:23

---
+++ Spring-Oauth2/src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -0,0 +1,11 @@
+package com.example.demo.security;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService {
+	
+}
