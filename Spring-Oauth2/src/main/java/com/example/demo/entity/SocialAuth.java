9f39d06a780cf1cfb7552a1e573b95346f5fc3a1
whodatt1 <codingman875@naver.com>
2023-11-28 21:40:03 +0900

    update:2023-11-28 21:40

---
+++ Spring-Oauth2/src/main/java/com/example/demo/entity/SocialAuth.java
@@ -0,0 +1,36 @@
+package com.example.demo.entity;
+
+import java.util.Map;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class SocialAuth {
+	private String providerId;
+	@Enumerated(value = EnumType.STRING)
+	private AuthProvider authProvider;
+	private String email;
+	private String name;
+	private String imageUrl;
+	private String attributes;
+	private String ip;
+	
+	public void update(String name, String imageUrl, Map<String, Object> attributes) {
+		this.name = name;
+		this.imageUrl = imageUrl;
+		this.attributes = attributes.toString();
+	}
+}
--- Spring-Oauth2/src/main/java/com/example/demo/entity/User.java
+++ Spring-Oauth2/src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,83 @@
 package com.example.demo.entity;
 
-public class User {
+import java.sql.Date;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SecondaryTable;
+import javax.persistence.SecondaryTables;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString(exclude = "socialAuth")
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "user", uniqueConstraints = {
+		@UniqueConstraint(columnNames = "email")
+})
+@SecondaryTables({
+	@SecondaryTable(name = "social_auth", pkJoinColumns = @PrimaryKeyJoinColumn(name = "user_id"))
+})
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id", nullable = false)
+	private long id;
+	
+	@Column(name = "email", length = 100, nullable = false)
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "nickname", length = 100, nullable = false)
+	private String nickname;
+	
+	@Column(name = "tel", length = 100)
+	private String tel;
+	
+	@CreationTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "regdt", nullable = false, updatable = false)
+	private Date regdt;
+	
+	@UpdateTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated", nullable = false)
+	private Date moddt;
+	
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name = "providerId", column = @Column(table = "social_auth", name = "provider_id")),
+		@AttributeOverride(name = "provider", column = @Column(table = "social_auth", name = "provider")),
+		@AttributeOverride(name = "email", column = @Column(table = "social_auth", name = "email", length = 100, nullable = false)),
+		@AttributeOverride(name = "name", column = @Column(table = "social_auth", name = "name", length = 100, nullable = false)),
+		@AttributeOverride(name = "imageUrl", column = @Column(table = "social_auth", name = "imageUrl", columnDefinition = "TEXT")),
+		@AttributeOverride(name = "attributes", column = @Column(table = "social_auth", name = "attributes", columnDefinition = "TEXT")),
+		@AttributeOverride(name = "ip", column = @Column(table = "social_auth", name = "ip", length = 100, nullable = false))
+	})
+	private SocialAuth socialAuth;
 }
