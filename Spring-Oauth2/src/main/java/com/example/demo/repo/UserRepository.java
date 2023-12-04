6744393979d8f97e6bd50e0d0fc2a37070e5c710
whodatt1 <codingman875@naver.com>
2023-12-04 22:37:50 +0900

    update:2023-12-04 22:37

--- Spring-Oauth2/src/main/java/com/example/demo/repo/UserRepository.java
+++ Spring-Oauth2/src/main/java/com/example/demo/repo/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	
+	Optional<User> findFirstByEmailOrderByIdAsc(String email);
 }
