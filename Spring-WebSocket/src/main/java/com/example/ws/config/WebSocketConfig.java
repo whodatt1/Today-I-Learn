32cbccc029cff350c6f0bf3f16fafaa273a7ea0b
whodatt1 <codingman875@naver.com>
2024-01-10 22:48:32 +0900

    update:2024-01-10 22:48

---
+++ Spring-WebSocket/src/main/java/com/example/ws/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.example.ws.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		// 연결 경로 설정. SockJS 사용하여 브라우저 등에서 웹소켓을 지원하지 않을 경우 대안 기술 사용할 수 있도록 설정
+		registry.addEndpoint("/websc").withSockJS(); 
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker("/topic");
+		registry.setApplicationDestinationPrefixes("/app");
+	}
+	
+}
