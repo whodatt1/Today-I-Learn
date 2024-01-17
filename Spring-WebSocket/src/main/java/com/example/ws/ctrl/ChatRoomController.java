74571f88817e6740e5d10469d659f337ce842866
whodatt1 <codingman875@naver.com>
2024-01-17 23:49:53 +0900

    update:2024-01-17 23:49

--- Spring-WebSocket/src/main/java/com/example/ws/ctrl/ChatRoomController.java
+++ Spring-WebSocket/src/main/java/com/example/ws/ctrl/ChatRoomController.java
@@ -1,8 +1,17 @@
 package com.example.ws.ctrl;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.ws.dto.ChatRoom;
 import com.example.ws.service.ChatService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +23,37 @@ public class ChatRoomController {
 	
 	private final ChatService chatService;
 	
+	@GetMapping("/room")
+	public String rooms(Model model) {
+		return "/main.html";
+	}
+	
+	// 모든 채팅방 목록 조회
+	@GetMapping("/roomlist")
+	@ResponseBody
+	public List<ChatRoom> roomList() {
+		return chatService.findAllRoom();
+	}
+	
+	// 채팅방 생성
+	@PostMapping("/room")
+	@ResponseBody
+	public ChatRoom createRoom(@RequestParam String name) {
+		return chatService.createRoom(name);
+	}
+	
+	// 채팅방 입장
+	@GetMapping("/room/enter/{roomId}")
+	@ResponseBody
+	public String roomEnter(Model model, @PathVariable String roomId) {
+		model.addAttribute("roomId", roomId);
+		return "/roominside.html";
+	}
+	
+	// 채팅방 디테일 조회
+	@GetMapping("/room/{roomId}")
+	@ResponseBody
+	public ChatRoom roomInside(@PathVariable String roomId) {
+		return chatService.findById(roomId);
+	}
 }
