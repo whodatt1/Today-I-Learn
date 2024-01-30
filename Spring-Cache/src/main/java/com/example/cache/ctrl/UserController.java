9d3d41a795e492c6d17919cea2623f7a1a45393c
whodatt1 <codingman875@naver.com>
2024-01-30 23:54:17 +0900

    update:2024-01-30 23:54

--- Spring-Cache/src/main/java/com/example/cache/ctrl/UserController.java
+++ Spring-Cache/src/main/java/com/example/cache/ctrl/UserController.java
@@ -1,8 +1,12 @@
 package com.example.cache.ctrl;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,5 +45,25 @@ public class UserController {
 		return user;
 	}
 	
+	@PostMapping("/user/ins")
+	public User save(@RequestBody User user) {
+		User newUser = userRepository.save(user);
+		log.info("Contoller save {}", newUser);
+		return newUser;
+	}
 	
+	@PutMapping("/user/mod")
+	public User update(@RequestBody User user) {
+		User modUser = userRepository.update(user);
+		log.info("Controller update {}", modUser);
+		return modUser;
+	}
+	
+	@DeleteMapping("/user/${userId}")
+	public void delete(@PathVariable String userId) {
+		Long realUserId = Long.parseLong(userId);
+		User delUser = userRepository.findById(realUserId);
+		log.info("Controller delete {}", delUser);
+		userRepository.delete(delUser);
+	}
 }
