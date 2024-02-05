9effba29890ae2c56ad574142929276bbd6b708e
whodatt1 <codingman875@naver.com>
2024-02-05 23:39:44 +0900

    update:2024-02-05 23:39

--- Spring-Cache/src/main/java/com/example/cache/ctrl/UserController.java
+++ Spring-Cache/src/main/java/com/example/cache/ctrl/UserController.java
@@ -2,7 +2,6 @@ package com.example.cache.ctrl;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
