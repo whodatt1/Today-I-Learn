b08d448e2277dbc5880fb5478ff78f782dc69f41
whodatt1 <codingman875@naver.com>
2024-03-18 23:40:26 +0900

    update:2024-03-18 23:40

--- Spring-Redis/src/main/java/com/example/redis/service/impl/RedisTemplateServiceImpl.java
+++ Spring-Redis/src/main/java/com/example/redis/service/impl/RedisTemplateServiceImpl.java
@@ -1,14 +1,16 @@
 package com.example.redis.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.example.redis.dto.Movie;
 import com.example.redis.exception.movie.MovieExistsException;
+import com.example.redis.exception.movie.MovieNotFoundException;
 import com.example.redis.repo.RedisTemplateRepository;
 import com.example.redis.service.RedisTemplateService;
 
@@ -18,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RedisTemplateServiceImpl implements RedisTemplateService {
 	
-	private final RedisTemplate<String, Movie> redisTemplate;
+	private final String hashReference = "Movie";
 	
 	private final RedisTemplateRepository redisTemplateRepository;
 	
@@ -34,12 +36,8 @@ public class RedisTemplateServiceImpl implements RedisTemplateService {
 		
 		if (movieChkDB.isPresent() && movieChkRedis.isPresent()) {
 			throw new MovieExistsException("Redis와 DB에 이미 존재하는 영화입니다.");
-		} else if (movieChkDB.isPresent() && !movieChkRedis.isPresent()) {
-			hashOperations.putIfAbsent("Movie", movie.getMovieCd(), movie);
-		} else if (!movieChkDB.isPresent() && movieChkRedis.isPresent()) {
-			redisTemplateRepository.save(movie);
 		} else {
-			hashOperations.putIfAbsent("Movie", movie.getMovieCd(), movie);
+			hashOperations.putIfAbsent(hashReference, movie.getMovieCd(), movie);
 			redisTemplateRepository.save(movie);
 		}
 		
@@ -53,27 +51,53 @@ public class RedisTemplateServiceImpl implements RedisTemplateService {
 		// Redis 데이터 체크
 		Optional<Movie> movieChkRedis = Optional.ofNullable(hashOperations.get("Movie", movie.getMovieCd()));
 		
+		if (!movieChkDB.isPresent() && !movieChkRedis.isPresent()) {
+			throw new MovieNotFoundException("Redis와 DB에 존재하지 않는 영화입니다.");
+		} else {
+			hashOperations.put(hashReference, movie.getMovieCd(), movie);
+			redisTemplateRepository.save(movie);
+		}
 		
-		
-		return null;
+		return movie;
 	}
 
 	@Override
 	public void deleteMovieWithTemp(String movieCd) {
-		// TODO Auto-generated method stub
+		// DB 데이터 체크
+		Optional<Movie> movieChkDB = redisTemplateRepository.findById(movieCd);
+		// Redis 데이터 체크
+		Optional<Movie> movieChkRedis = Optional.ofNullable(hashOperations.get(hashReference, movieCd));
 		
+		if (!movieChkDB.isPresent() && !movieChkRedis.isPresent()) {
+			throw new MovieNotFoundException("Redis와 DB에 존재하지 않는 영화입니다.");
+		} else {
+			hashOperations.delete(hashReference, movieChkRedis.get().getMovieCd());
+			redisTemplateRepository.delete(movieChkDB.get());
+		}
 	}
 
 	@Override
 	public List<Movie> getMovieListAllWithTemp() {
-		// TODO Auto-generated method stub
-		return null;
+		// 적당한 리스트 크기라 가정하여 Redis 에서 가져오는 것으로 구현
+		Map<String, Movie> entries = hashOperations.entries(hashReference);
+		List<Movie> movieList = new ArrayList<>(entries.values());
+		
+		return movieList;
 	}
 
 	@Override
 	public Movie getMovieDetailByIdWithTemp(String movieCd) {
-		// TODO Auto-generated method stub
-		return null;
+		// // DB 데이터 가져오기
+		Optional<Movie> movieDetDB = redisTemplateRepository.findById(movieCd);
+		// Redis 데이터 가져오기
+		Optional<Movie> movieDetRedis = Optional.ofNullable(hashOperations.get(hashReference, movieCd));
+		
+		// Redis에 저장된 값이 있다면 Redis에서 가져옴
+		if (movieDetRedis.isPresent()) {
+			return movieDetRedis.get();
+		} else {
+			return movieDetDB.get();
+		}
 	}
 
 }
