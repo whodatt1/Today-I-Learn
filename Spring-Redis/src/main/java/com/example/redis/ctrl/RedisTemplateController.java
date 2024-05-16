1670234db8e9c118670af3a30fe693e6ed01b12a
whodatt1 <codingman875@naver.com>
2024-05-16 21:14:14 +0900

    update:2024-05-16 21:14

--- Spring-Redis/src/main/java/com/example/redis/ctrl/RedisTemplateController.java
+++ Spring-Redis/src/main/java/com/example/redis/ctrl/RedisTemplateController.java
@@ -3,6 +3,8 @@ package com.example.redis.ctrl;
 import java.util.HashMap;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.redis.dto.Movie;
+import com.example.redis.exception.user.UserNotFoundException;
 import com.example.redis.service.RedisTemplateService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class RedisTemplateController {
@@ -32,31 +37,65 @@ public class RedisTemplateController {
 	}
 	
 	@PostMapping("/redistemp/ins")
-	public Movie insertMovieWithTemp(@RequestBody Movie movie) {
-		Movie newMovie = redisTemplateService.insertMovieWithTemp(movie);
-		return newMovie;
+	public ResponseEntity<?> insertMovieWithTemp(@RequestBody Movie movie) {
+		try {
+			Movie newMovie = redisTemplateService.insertMovieWithTemp(movie);
+			
+			log.info("Contoller insertMovieWithTemp");
+			
+			return new ResponseEntity<>(newMovie, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
 	
 	@PutMapping("/redistemp/upd")
-	public Movie updateMovieWithTemp(@RequestBody Movie movie) {
-		Movie updMovie = redisTemplateService.updateMovieWithTemp(movie);
-		return updMovie;
+	public ResponseEntity<?> updateMovieWithTemp(@RequestBody Movie movie) {
+		try {
+			Movie updMovie = redisTemplateService.updateMovieWithTemp(movie);
+			
+			log.info("Contoller updateMovieWithTemp");
+			
+			return new ResponseEntity<>(updMovie, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
 	
 	@PutMapping("/redistemp/del")
-	public void deleteMovieWithTemp(@RequestBody Movie movie) {
-		redisTemplateService.deleteMovieWithTemp(movie);
+	public ResponseEntity<?> deleteMovieWithTemp(@RequestBody Movie movie) {
+		try {
+			Movie delMovie = redisTemplateService.deleteMovieWithTemp(movie);
+			
+			log.info("Contoller deleteMovieWithTemp");
+			
+			return new ResponseEntity<>(delMovie, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
 	
 	@GetMapping("/redistemp/all")
-	public List<Movie> getMovieListAllWithTemp(@RequestParam HashMap<String, Object> params) {
+	public ResponseEntity<?> getMovieListAllWithTemp(@RequestParam HashMap<String, Object> params) {
 		List<Movie> mList = redisTemplateService.getMovieListAllWithTemp(params);
-		return mList;
+		
+		if (mList == null || mList.size() == 0) {
+			return new ResponseEntity<>("조회할 영화가 없습니다!", HttpStatus.BAD_REQUEST);
+		} else {
+			return new ResponseEntity<>(mList, HttpStatus.OK);
+		}
 	}
 	
 	@GetMapping("/redistemp/detail/{movieCd}")
-	public Movie getMovieDetailByIdWithTemp(@PathVariable String movieCd) {
-		Movie detMovie = redisTemplateService.getMovieDetailByIdWithTemp(movieCd);
-		return detMovie;
+	public ResponseEntity<?> getMovieDetailByIdWithTemp(@PathVariable String movieCd) {
+		try {
+			Movie detMovie = redisTemplateService.getMovieDetailByIdWithTemp(movieCd);
+			
+			log.info("Contoller getMovieDetailByIdWithTemp");
+			
+			return new ResponseEntity<>(detMovie, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
 }
