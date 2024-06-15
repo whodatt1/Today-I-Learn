5dd3f31b45d3797d375c996468018c2270ff75a5
whodatt1 <codingman875@naver.com>
2024-06-15 12:51:22 +0900

    update:2024-06-15 12:51

---
+++ Spring-Flux-Test/src/main/java/com/example/fluxtest/SpringFluxTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.fluxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFluxTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFluxTestApplication.class, args);
+	}
+
+}
