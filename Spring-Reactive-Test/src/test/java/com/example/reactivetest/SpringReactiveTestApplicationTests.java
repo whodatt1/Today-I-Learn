6b8b38a4b8be1f68e75333c0db46053650e6354d
whodatt1 <codingman875@naver.com>
2024-06-15 13:32:35 +0900

    update:2024-06-15 13:32

---
+++ Spring-Reactive-Test/src/test/java/com/example/reactivetest/SpringReactiveTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
